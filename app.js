7f04cc282b12d73bb5221868535e9f88ed799910
Yave-Vargas <90014627+Yave-Vargas@users.noreply.github.com>
2023-05-07 23:03:55 -0600

    Deploy con Node.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const hbs = require("hbs");
+const bodyParser = require("body-parser");
+const cors = require("cors"); 
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/view',()=>{});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Definir las Rutas - Para el cliente: login, categorias, index, not found
+//GET o POST
+app.get('/',(req, res)=>{
+    res.render('index')
+})
+app.get('/login',(req, res)=>{
+    res.render('signin-one')
+})
+app.get('/registro',(req, res)=>{
+    res.render('registro')
+})
+app.get('*',(req, res)=>{
+    res.render('404')
+})
+//Advanced Tables
+//sign in one
+//404
+
+//Definir puerto en el que se escucha solucitudes
+app.listen(port,()=>{
+    console.log('El servidor express esta corriendo en el puerto: ',port);
+}) 
